2abbd9e8d154f9a304d8a4545923fe70dbbcac24
Trương Hoàng Phi <hoangphit28@gmail.com>
2026-01-29 14:23:46 +0700

    new fix

--- script.js
+++ script.js
@@ -4,53 +4,59 @@ let products = [];
 let displayList = [];
 
 fetch(API_URL)
-    .then(res => res.json())
-    .then(data => {
-        products = data.products;
-        displayList = [...products];
-        render(displayList);
-    });
+  .then(res => res.json())
+  .then(data => {
+    products = data.products;
+    displayList = [...products];
+    render(displayList);
+  })
+  .catch(err => console.error("Lỗi load JSON:", err));
 
 function render(list) {
-    const tbody = document.getElementById("product-table");
-    tbody.innerHTML = "";
+  const tbody = document.getElementById("product-table");
+  tbody.innerHTML = "";
 
-    list.forEach(p => {
-        tbody.innerHTML += `
-            <tr>
-                <td>${p.id}</td>
-                <td>${p.name}</td>
-                <td>${p.price.toLocaleString()}</td>
-            </tr>
-        `;
-    });
+  list.forEach(p => {
+    tbody.innerHTML += `
+      <tr>
+        <td>${p.id}</td>
+        <td>
+          <img src="${p.image}" 
+               width="60" height="60"
+               class="rounded border">
+        </td>
+        <td>${p.name}</td>
+        <td>${p.price.toLocaleString()} đ</td>
+      </tr>
+    `;
+  });
 }
-
 
 function onChanged(keyword) {
-    displayList = products.filter(p =>
-        p.name.toLowerCase().includes(keyword.toLowerCase())
-    );
-    render(displayList);
+  displayList = products.filter(p =>
+    p.name.toLowerCase().includes(keyword.toLowerCase())
+  );
+  render(displayList);
 }
 
+
 function sortNameAsc() {
-    displayList.sort((a, b) => a.name.localeCompare(b.name));
-    render(displayList);
+  displayList.sort((a, b) => a.name.localeCompare(b.name));
+  render(displayList);
 }
 
 function sortNameDesc() {
-    displayList.sort((a, b) => b.name.localeCompare(a.name));
-    render(displayList);
+  displayList.sort((a, b) => b.name.localeCompare(a.name));
+  render(displayList);
 }
 
 
 function sortPriceAsc() {
-    displayList.sort((a, b) => a.price - b.price);
-    render(displayList);
+  displayList.sort((a, b) => a.price - b.price);
+  render(displayList);
 }
 
 function sortPriceDesc() {
-    displayList.sort((a, b) => b.price - a.price);
-    render(displayList);
+  displayList.sort((a, b) => b.price - a.price);
+  render(displayList);
 }
